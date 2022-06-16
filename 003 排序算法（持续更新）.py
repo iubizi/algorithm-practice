7969cd194a7caa009217f2dce80d2f38ebeb87ea
iubizi <106417012+iubizi@users.noreply.github.com>
2022-06-16 15:42:53 -0400

    Add files via upload

--- 003 排序算法（持续更新）.py
+++ 003 排序算法（持续更新）.py
@@ -67,6 +67,34 @@ def shell_sort(arr):
 
     return arr
 
+####################
+# 桶
+####################
+
+def bucket_sort(arr, bucket_num=5): # 默认五桶
+
+    arr_min = min(arr)
+    arr_max = max(arr)
+
+    # bucket_num = len(arr) # 空间换时间
+
+    bucket_size = (arr_max - arr_min + 1) // bucket_num
+
+    bucket = [ [] for _ in range(bucket_num) ] # 2D方便
+
+    for i in arr: # 分桶
+        index = (i - arr_min) // bucket_size
+        bucket[index].append(i)
+
+    for i in bucket: # 桶内排序，使用快排或者归并
+        i.sort() # 稳定排序方法
+
+    ans = [] # 输出
+    for i in bucket: # 拆掉数组合并在一起
+        ans.extend(i)
+
+    return ans
+
 
 
 ####################
@@ -100,5 +128,7 @@ if __name__ == '__main__':
     print(select_sort(arr))
     # 希尔
     print(shell_sort(arr))
+    # 桶
+    print(bucket_sort(arr, len(arr))) # 空间换时间
 
-    val(shell_sort) # 对分器
+    val(bucket_sort) # 对分器
